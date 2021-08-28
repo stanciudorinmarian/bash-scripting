a7697b4c1d8113abee378d562a69505b55c2ffd7
Dorin Stanciu <stanciudorinmarian@gmail.com>
2021-08-28 22:04:28 +0300

    Bash Scripting - Local vs Global

--- learning/functions.sh
+++ learning/functions.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Learning basic functions call
 function isYoungerOrOlder() {
   if [[ (-z "$1") || (-z "$2") ]]; then
     echo -e "Invalid function parameters. \n"
@@ -24,7 +25,35 @@ function isYoungerOrOlder() {
   echo -e "They are the same age.\n"
 }
 
-isYoungerOrOlder 13
-isYoungerOrOlder 13 13
-isYoungerOrOlder 24 13
-isYoungerOrOlder 13 24
+#isYoungerOrOlder 13
+#isYoungerOrOlder 13 13
+#isYoungerOrOlder 24 13
+#isYoungerOrOlder 13 24
+
+# Learning local vs global vars
+GLOBAL_VAR="global var"
+
+function tryToOverwriteGlobalVar() {
+  if [[ (-z "$1") ]]; then
+    echo -e "Invalid function parameters. \n"
+    return 1
+  fi
+
+  shouldOverWrite=$1
+
+  if $shouldOverWrite; then
+    GLOBAL_VAR="globally overwritten"
+  elif ! $shouldOverWrite; then
+    local GLOBAL_VAR="locally overwritten"
+
+    echo "Locally declared var with name GLOBAL_VAR and value $GLOBAL_VAR";
+  fi
+}
+
+tryToOverwriteGlobalVar false
+
+echo "Globally declared var should have the same value. Value: $GLOBAL_VAR"
+
+tryToOverwriteGlobalVar true
+
+echo "Globally declared var should be overwritten. Value: $GLOBAL_VAR"
